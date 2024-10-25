e54ef3bf75c2ed9a81fcbbba715b7abbcdb5e752
Pasola01 <pasolchik@gmail.com>
2024-10-25 19:57:24 +0300

    fixed structure

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,20 +2,23 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    # Локатори
-    username_input_locator = "#username"
-    password_input_locator = "#password"
-    submit_button_locator = "//button[@type='submit' and @class='btn btn-bg btn-primary d-block w-100']"
-    login_flash_message = '#flash b'
+    # Locators
+    login_username_input_locator = "//input[@id='username']"
+    login_password_input_locator = "//input[@id='password']"
+    submit_button_locator = "//button[@type='submit']"
+    logout_btn_locator = "//i[@class='icon-2x icon-signout']"
 
     def enter_username(self, username):
-        self.page.locator(self.username_input_locator).fill(username)  # Прямий доступ
+        self.page.locator(self.login_username_input_locator).fill(username)
 
     def enter_password(self, password):
-        self.page.locator(self.password_input_locator).fill(password)
+        self.page.locator(self.login_password_input_locator).fill(password)
 
     def click_submit(self):
         self.page.locator(self.submit_button_locator).click()
 
     def click_submit_by_role(self):
-        self.page.get_by_role("button", name="Submit").click()  # Використання get_by_role
+        self.page.get_by_role("button", name="Submit").click()
+
+    def click_logout(self):
+        self.page.locator(self.logout_btn_locator).click()

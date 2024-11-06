0cb64f23a65b8d21bec968af9a477566d9e72a43
YuriiMelika <yura.melika@gmail.com>
2024-11-06 17:04:07 +0200

    added tests for add_remove_elements

--- pages/add_remove_elements_page.py
+++ pages/add_remove_elements_page.py
@@ -2,7 +2,11 @@ from pages.base_page import BasePage
 
 class AddRemoveElements(BasePage):
     add_element_locator = "//button[@class='btn btn-primary mt-3']"
+    added_element_locator = "//button[@class='added-manually btn btn-info']"
 
 
-    def click_add_element(self):
+    def click_add_element_btn(self):
         self.page.locator(self.add_element_locator).click()
+
+    def remove_element(self):
+        self.page.locator(self.added_element_locator).click()
--- tests/test_add_remove_elements.py
+++ tests/test_add_remove_elements.py
@@ -1,6 +1,23 @@
 from pages.add_remove_elements_page import AddRemoveElements
+from  playwright.sync_api import expect
 
-def test_add_remove_elements(page, config):
+def test_add_remove_elements_add_element(page, config):
     add_remove_elements_page = AddRemoveElements(page)
     add_remove_elements_page.page.goto(config['url'] + "/add-remove-elements")
-    add_remove_elements_page.add_element_locator
+    add_remove_elements_page.click_add_element_btn()
+    expect(page.locator(add_remove_elements_page.added_element_locator)).to_be_visible()
+
+def test_add_remove_elements_add_few_elements(page, config):
+    add_remove_elements_page = AddRemoveElements(page)
+    add_remove_elements_page.page.goto(config['url'] + "/add-remove-elements")
+    add_remove_elements_page.click_add_element_btn()
+    add_remove_elements_page.click_add_element_btn()
+    add_remove_elements_page.click_add_element_btn()
+    expect(page.locator(add_remove_elements_page.added_element_locator)).to_have_count(3)
+
+def test_add_remove_elements_remove_element(page, config):
+    add_remove_elements_page = AddRemoveElements(page)
+    add_remove_elements_page.page.goto(config['url'] + "/add-remove-elements")
+    add_remove_elements_page.click_add_element_btn()
+    add_remove_elements_page.remove_element()
+    expect(page.locator(add_remove_elements_page.added_element_locator)).not_to_be_visible()
